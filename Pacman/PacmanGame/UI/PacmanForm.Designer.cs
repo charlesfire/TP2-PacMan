a1db663a91df3486c1cf6eafea21e10499ce8ff7
Charlesfire <charlesfire2@hotmail.com>
2015-04-07 10:54:02 -0400

    Ajout de l'icone

--- Pacman/PacmanGame/UI/PacmanForm.Designer.cs
+++ Pacman/PacmanGame/UI/PacmanForm.Designer.cs
@@ -29,6 +29,7 @@
     private void InitializeComponent( )
     {
       this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
       this.mainTimer = new System.Windows.Forms.Timer(this.components);
       this.SuspendLayout();
       // 
@@ -41,11 +42,13 @@
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.BackColor = System.Drawing.Color.White;
+      this.BackColor = System.Drawing.Color.Black;
       this.ClientSize = new System.Drawing.Size(384, 362);
       this.DoubleBuffered = true;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Name = "Form1";
-      this.Text = "Form 1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Pacman";
       this.Load += new System.EventHandler(this.PacmanForm_Load);
       this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnDraw);
       this.ResumeLayout(false);
