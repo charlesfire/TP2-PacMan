202965d26faecee58b5122291fad5dcb731fab0b
Charlesfire <charlesfire2@hotmail.com>
2015-04-10 13:19:40 -0400

    Amélioration de la form

--- Pacman/PacmanGame/Logic/PacmanGame.cs
+++ Pacman/PacmanGame/Logic/PacmanGame.cs
@@ -11,22 +11,29 @@ namespace Pacman
     //Largeur du formulaire du jeu
     public const int GAME_WIDTH = 1;
     //Pacman du jeu
-    ////////////////////////////////Pacman pacman = new Pacman();
+    Pacman pacman = new Pacman();
+
     //Nombre de fantômes dans le jeu
     private int NB_GHOSTS = 4;
+
     //Tableau des fantômes dans le jeu
-    private Ghost[] ghosts = new Ghost[4];
+    private Ghost[] ghosts = new Ghost[NB_GHOSTS];
+
     //...? (à compléter)
     private bool[] pills = new bool[1];
     //</Tommy Bouffard>
 
+    //<Charles Lachance>
     /// <summary>
     /// Constructeur du jeu de Pacman
     /// </summary>
     public PacmanGame()
     {
-
+      ghosts = new Ghost[NB_GHOSTS];
+      pills = new bool[1];
     }
+    //</Charles Lachance>
+
     // Vous aurez probablement à modifier le type de retour ici pour pouvoir
     // tester la fin de partie.  Par contre, pour vous donner un projet qui
     // compile, je n'avais pas le choix de mettre "void".
--- Pacman/PacmanGame/UI/PacmanForm.Designer.cs
+++ Pacman/PacmanGame/UI/PacmanForm.Designer.cs
@@ -45,7 +45,10 @@
       this.BackColor = System.Drawing.Color.Black;
       this.ClientSize = new System.Drawing.Size(384, 362);
       this.DoubleBuffered = true;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
       this.Name = "Form1";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Pacman";
